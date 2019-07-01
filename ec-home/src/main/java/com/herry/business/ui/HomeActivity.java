56cd99f08e544346c4ddb2af6fc86a0b834a8c4c
Herry <1033857564@qq.com>
2019-07-02 01:54:02 +0800

    模块化支持

---
+++ ec-home/src/main/java/com/herry/business/ui/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.herry.business.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.herry.business.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_layout);
+    }
+}
